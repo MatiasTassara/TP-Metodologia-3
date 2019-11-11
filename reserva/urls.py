424767a95db273ba88fa76120cccd45c7924abc3
MatiasTassara <matiastassara59@gmail.com>
2019-11-11 20:25:57 -0300

    Trabajo en la facu

--- reserva/urls.py
+++ reserva/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('info/<int:id>',views.info,name='info'),
 ]
--- reserva/views.py
+++ reserva/views.py
@@ -4,3 +4,7 @@ from .models import RealState
 def home(request):
     realStates = RealState.objects.all()
     return render(request, 'home.html', {'realStates': realStates})
+
+def info(request,id=0):
+    realState = RealState.objects.get(pk=id)
+    return render(request, 'info.html', {'realState': realState})
